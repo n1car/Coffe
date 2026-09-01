d17f1dbedce0c399232e7c5ae95e4dc9409894ce
Nicolas Cardenas Guaca <1109187838@u.icesi.edu.co>
2026-08-31 20:06:06 -0500

    commit 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,36 @@
 import { Module } from '@nestjs/common';
-import { createObserveModule } from '@nestjs/observe';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { OrdersModule } from './orders/orders.module.js';
 import { LoansModule } from './loans/loans.module.js';
 
-export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'coffee-orders-api',
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST') ?? 'localhost',
+        port: Number(configService.get<string>('DB_PORT') ?? 5432),
+        username: configService.get<string>('DB_USERNAME') ?? 'postgres',
+        password: configService.get<string>('DB_PASSWORD') ?? 'postgres',
+        database:
+          configService.get<string>('DB_DATABASE') ?? 'coffee_orders',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     OrdersModule,
-    LoansModule,
+    LoansModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller.js';
 import { OrdersService } from './orders.service.js';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerEntity } from './entities/customer.entity.js';
+import { OrderEntity } from './entities/order.entity.js';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CustomerEntity, OrderEntity])],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
-export class OrdersModule {}
+export class OrdersModule { }
